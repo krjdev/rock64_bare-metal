6826894573220885dea6620468f485ff0b227309
Johannes Krottmayer <krjdev@gmail.com>
2020-06-05 14:44:16 +0200

    add RK3328 CRU Driver

---
+++ include/dev/cru.h
@@ -0,0 +1,24 @@
+/**
+ *
+ * File Name: dev/cru.h
+ * Title    : RK3328 CRU (Clock and Reset Unit) Driver
+ * Project  : 
+ * Author   : Copyright (C) 2020 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2020-06-05
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#ifndef DEV_CRU_H
+#define DEV_CRU_H
+
+extern void cru_reset(void);
+
+#endif

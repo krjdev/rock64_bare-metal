d87f3cb9247341ac888ef30ebe34263e1d96b790
Johannes Krottmayer <krjdev@gmail.com>
2020-06-06 10:07:15 +0200

    add CPU functions

---
+++ include/cpu.h
@@ -0,0 +1,25 @@
+/**
+ *
+ * File Name: cpu.h
+ * Title    : AArch64 CPU Functions
+ * Project  : 
+ * Author   : Copyright (C) 2020 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2020-06-06
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#ifndef CPU_H
+#define CPU_H
+
+extern int cpu_get_cur_el(void);
+extern int cpu_get_cur_id(void);
+
+#endif

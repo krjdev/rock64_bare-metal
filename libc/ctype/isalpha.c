cee5957f9b0b39f2326be2415e484ca708461b8a
Johannes Krottmayer <krjdev@gmail.com>
2021-01-23 14:27:09 +0100

    libc/ctype: Add initial ctype functions
    New file(s):
    
    ./libc/ctype/isalnum.c
    ./libc/ctype/isalpha.c
    ./libc/ctype/isblank.c
    ./libc/ctype/iscntrl.c
    ./libc/ctype/isdigit.c
    ./libc/ctype/isgraph.c
    ./libc/ctype/islower.c
    ./libc/ctype/isprint.c
    ./libc/ctype/ispunct.c
    ./libc/ctype/isspace.c
    ./libc/ctype/isupper.c
    ./libc/ctype/isxdigit.c
    ./libc/ctype/tolower.c
    ./libc/ctype/toupper.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libc/ctype/isalpha.c
@@ -0,0 +1,26 @@
+/**
+ *
+ * File Name: libc/ctype/isalpha.c
+ * Title    : Simple Standard C Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-16
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+int isalpha(const int c)
+{
+    if (((c >= 'A') && (c <= 'Z')) || 
+        ((c >= 'a') && (c <= 'z')))
+        return 1;
+    
+    return 0;
+}
---
+++ libc/ctype/isdigit.c
@@ -0,0 +1,25 @@
+/**
+ *
+ * File Name: libc/ctype/isdigit.c
+ * Title    : Simple Standard C Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-16
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+int isdigit(const int c)
+{
+    if ((c >= '0') && (c <= '9'))
+        return 1;
+    
+    return 0;
+}

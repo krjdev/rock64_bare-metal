3947b3d006d80bfe85606b74881add67ace7367b
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 15:11:16 +0100

    libkern/stdlib: Add some stdlib functions
    New file(s):
    
    ./libkern/stdlib/abort.c
    ./libkern/stdlib/free.c
    ./libkern/stdlib/malloc.c
    ./libkern/stdlib/realloc.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libkern/stdlib/malloc.c
@@ -0,0 +1,28 @@
+/**
+ *
+ * File Name: libkern/stdlib/malloc.c
+ * Title    : Kernel Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-25
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#include <stdlib.h>
+#include <heap.h>
+
+void *malloc(size_t len)
+{
+    if (len < 1)
+        return NULL;
+    
+    return heap_alloc(len);
+}
---
+++ libkern/stdlib/realloc.c
@@ -0,0 +1,46 @@
+/**
+ *
+ * File Name: libkern/stdlib/realloc.c
+ * Title    : Kernel Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-25
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <heap.h>
+
+void *realloc(void *ptr, size_t len)
+{
+    int size;
+    void *p;
+    
+    if (!ptr)
+        return NULL;
+    
+    if (len < 1)
+        return NULL;
+    
+    size = heap_size(ptr);
+    
+    if (!size)
+        return NULL;
+    
+    p = heap_alloc(len);
+    
+    if (!p)
+        return NULL;
+    
+    memcpy(p, ptr, size);
+    return p;
+}

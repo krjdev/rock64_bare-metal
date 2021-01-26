335a646b5ba704c50993986c45cc60569331d946
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 17:54:58 +0100

    kern: Add initial Kernel init
    New file(s):
    
    ./kern/kern_init.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ kern/kern_init.c
@@ -0,0 +1,26 @@
+/**
+ *
+ * File Name: kern/kern_init.c
+ * Title    : Kernel
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-25
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#include <init.h>
+#include <kern/main.h>
+
+void kern_init(void)
+{
+    libkern_init();
+    kern_main();
+}

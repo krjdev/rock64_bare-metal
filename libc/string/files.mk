f337ef98f95cad98bf3d4568472a536c1bbb6939
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 19:53:38 +0100

    libc/string: Add local Makefile
    New file(s):
    
    ./libc/string/files.mk
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libc/string/files.mk
@@ -0,0 +1,16 @@
+# PINE64 ROCK64 Bare-Metal
+#
+# C Library
+# 
+# libc/string
+
+CSRC_USER	+= libc/string/memcmp.c
+CSRC_USER	+= libc/string/memcpy.c
+CSRC_USER	+= libc/string/memset.c
+CSRC_USER	+= libc/string/strcat.c
+CSRC_USER	+= libc/string/strcmp.c
+CSRC_USER	+= libc/string/strcpy.c
+CSRC_USER	+= libc/string/strlen.c
+CSRC_USER	+= libc/string/strncat.c
+CSRC_USER	+= libc/string/strncmp.c
+CSRC_USER	+= libc/string/strncpy.c

da75a659ae894219f471d06258e3d5c8c028fca2
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 20:54:25 +0100

    asm: Some modifications
    New file(s):
    
    ./asm/intr.S
    
    Changed file(s):
    
    ./asm/files.mk
    ./asm/tlb.S
    ./asm/vector.S
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

--- asm/files.mk
+++ asm/files.mk
@@ -1,10 +1,19 @@
 # PINE64 ROCK64 Bare-Metal
 #
+# Assembler
+# 
 # asm
 
-ASRC	+= asm/vector.S
-ASRC	+= asm/start.S
-ASRC	+= asm/cache.S
-ASRC	+= asm/self.S
-ASRC	+= asm/tlb.S
+# Kernel
+ASRC_KERN	+= asm/vector.S
+ASRC_KERN	+= asm/start_kern.S
+ASRC_KERN	+= asm/cache.S
+ASRC_KERN	+= asm/self_kern.S
+ASRC_KERN	+= asm/tlb.S
+ASRC_KERN	+= asm/intr.S
+ASRC_KERN	+= asm/syscall_kern.S
 
+# User
+ASRC_USER	+= asm/start_user.S
+ASRC_USER	+= asm/self_user.S
+ASRC_USER	+= asm/syscall_user.S

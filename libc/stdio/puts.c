78eb1b8c204b3cc63f23f71d02cb39dec14453a1
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 21:05:53 +0100

    libc/stdio/puts.c: FIX compiler warning
    Changed file(s):
    
    ./libc/stdio/puts.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

--- libc/stdio/puts.c
+++ libc/stdio/puts.c
@@ -5,9 +5,9 @@
  * Project  : PINE64 ROCK64 Bare-Metal
  * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2021-01-25
- * Modified : 
+ * Modified : 2012-01-26
  * Revised  : 
- * Version  : 0.1.0.0
+ * Version  : 0.1.0.1
  * License  : ISC (see LICENSE.txt)
  *
  * NOTE: This code is currently below version 1.0, and therefore is considered
@@ -17,9 +17,10 @@
  */
 
 #include <string.h>
+#include <stdint.h>
 #include <unistd.h>
 
 int puts(const char *s)
 {
-    return write(1, s, strlen(s));
+    return write(1, (const uint8_t *) s, strlen(s));
 }

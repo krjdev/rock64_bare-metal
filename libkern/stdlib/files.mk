0e5d6b6c3d9ded169e526ca16a50b2dbc9816d34
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 15:12:12 +0100

    libkern/stdlib: Add local Makefile
    New file(s):
    
    ./libkern/stdlib/files.mk
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libkern/stdlib/files.mk
@@ -0,0 +1,10 @@
+# PINE64 ROCK64 Bare-Metal
+#
+# Kernel Library
+# 
+# libkern/stdlib
+
+CSRC_KERN	+= libkern/stdlib/abort.c
+CSRC_KERN	+= libkern/stdlib/free.c
+CSRC_KERN	+= libkern/stdlib/malloc.c
+CSRC_KERN	+= libkern/stdlib/realloc.c

d21f9f6c32aba5c4c6071cbe297f2724aa4284ea
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 19:56:50 +0100

    libc/stdlib: Add atoi()
    New file(s):
    
    ./libc/string/atoi.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libc/stdlib/atoi.c
@@ -0,0 +1,58 @@
+/**
+ *
+ * File Name: libc/stdlib/atoi.c
+ * Title    : C Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-23
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#include <string.h>
+#include <ctype.h>
+
+int atoi(const char *s)
+{
+    int len;
+    int i;
+    int sign = 0;
+    int ret = 0;
+    int tmp;
+    int mul = 1;
+    
+    if (!s)
+        return 0;
+    
+    len = strlen(s);
+    
+    if (s[0] == '-') {
+        sign = 1;
+        len--;
+    }
+    
+    for (i = len; i >= 0; i--) {
+        if (isdigit(s[i])) {
+            tmp = s[i] - '0';
+            tmp *= mul;
+            ret += tmp;
+            mul *= 10;
+            
+        } else
+            return 0;
+    }
+    
+    if (sign) {
+        ret--;
+        ret = ~ret;
+    }
+    
+    return ret;
+}

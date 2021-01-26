074c0fc06b9764ee22daa429e6d914b130bf2bf3
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 15:08:30 +0100

    libkern/ctype: Add local Makefile
    New file(s):
    
    ./libkern/ctype/files.mk
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libkern/ctype/files.mk
@@ -0,0 +1,20 @@
+# PINE64 ROCK64 Bare-Metal
+#
+# Kernel Library
+# 
+# libkern/ctype
+
+CSRC_KERN	+= libkern/ctype/isalnum.c
+CSRC_KERN	+= libkern/ctype/isalpha.c
+CSRC_KERN	+= libkern/ctype/isblank.c
+CSRC_KERN	+= libkern/ctype/iscntrl.c
+CSRC_KERN	+= libkern/ctype/isdigit.c
+CSRC_KERN	+= libkern/ctype/isgraph.c
+CSRC_KERN	+= libkern/ctype/islower.c
+CSRC_KERN	+= libkern/ctype/isprint.c
+CSRC_KERN	+= libkern/ctype/ispunct.c
+CSRC_KERN	+= libkern/ctype/isspace.c
+CSRC_KERN	+= libkern/ctype/isupper.c
+CSRC_KERN	+= libkern/ctype/isxdigit.c
+CSRC_KERN	+= libkern/ctype/tolower.c
+CSRC_KERN	+= libkern/ctype/toupper.c

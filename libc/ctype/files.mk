763f6040a70e163538b72d6f2b13838d2e2c42da
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 20:11:46 +0100

    libc/ctype: Add local Makefile
    New file(s):
    
    ./libc/ctype/files.mk
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libc/ctype/files.mk
@@ -0,0 +1,20 @@
+# PINE64 ROCK64 Bare-Metal
+#
+# C Library
+# 
+# libc/ctype
+
+CSRC_USER	+= libc/ctype/isalnum.c
+CSRC_USER	+= libc/ctype/isalpha.c
+CSRC_USER	+= libc/ctype/isblank.c
+CSRC_USER	+= libc/ctype/iscntrl.c
+CSRC_USER	+= libc/ctype/isdigit.c
+CSRC_USER	+= libc/ctype/isgraph.c
+CSRC_USER	+= libc/ctype/islower.c
+CSRC_USER	+= libc/ctype/isprint.c
+CSRC_USER	+= libc/ctype/ispunct.c
+CSRC_USER	+= libc/ctype/isspace.c
+CSRC_USER	+= libc/ctype/isupper.c
+CSRC_USER	+= libc/ctype/isxdigit.c
+CSRC_USER	+= libc/ctype/tolower.c
+CSRC_USER	+= libc/ctype/toupper.c

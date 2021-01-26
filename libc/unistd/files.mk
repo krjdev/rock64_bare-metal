41b1d3dd810e57909bccac49cb842156151ecf4a
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 20:09:33 +0100

    libc/unistd: Add write() and local Makefile
    New file(s):
    
    ./libc/unistd/files.mk
    ./libc/unistd/write.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libc/unistd/files.mk
@@ -0,0 +1,7 @@
+# PINE64 ROCK64 Bare-Metal
+#
+# C Library
+# 
+# libc/unistd
+
+CSRC_USER	+= libc/unistd/write.c
---
+++ libc/unistd/write.c
@@ -0,0 +1,24 @@
+/**
+ *
+ * File Name: libc/unistd/write.c
+ * Title    : C Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-26
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#include <syscall.h>
+
+int write(int fd, const uint8_t *buf, size_t len)
+{
+    return sys_write(fd, buf, len);
+}

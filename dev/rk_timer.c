049fd1f72d450f34dd87ee1a7468619ecead259a
Johannes Krottmayer <krjdev@gmail.com>
2021-01-26 20:32:05 +0100

    dev: Add device drivers
    New file(s):
    
    ./dev/files.mk
    ./dev/gic.c
    ./dev/init.c
    ./dev/rk_cru.c
    ./dev/rk_timer.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ dev/rk_timer.c
@@ -0,0 +1,24 @@
+/**
+ *
+ * File Name: dev/rk_timer.c
+ * Title    : Rockchip RK3328 Timer
+ * Project  : PINE64 ROCK64 Bare-Metal Example
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-26
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#include <errno.h>
+
+int rk_timer_init(void)
+{
+    return ESUCCESS;
+}

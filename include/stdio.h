05b99398661e152b2bc49387b56277ad2d5fc098
Johannes Krottmayer <krjdev@gmail.com>
2021-01-29 13:47:59 +0100

    include: Add inital Userland libc headers
    New file(s):
    ./include/ctype.h
    ./include/stddef.h
    ./include/stdio.h
    ./include/stdlib.h
    ./include/string.h
    ./include/unistd.h
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ include/stdio.h
@@ -0,0 +1,24 @@
+/**
+ *
+ * File Name: include/stdio.h
+ * Title    : C Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-29
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#ifndef ROCK64_STDIO_H
+#define ROCK64_STDIO_H
+
+extern int puts(const char *s);
+
+#endif

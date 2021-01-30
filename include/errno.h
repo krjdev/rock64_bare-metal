e04a7228cb7903fec58ecc9be59bb535754c2a86
Johannes Krottmayer <krjdev@gmail.com>
2021-01-30 21:35:46 +0100

    include: Add inital errno
    NEW FILE(S):
    ./include/errno.h
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ include/errno.h
@@ -0,0 +1,27 @@
+/**
+ *
+ * File Name: include/errno.h
+ * Title    : C Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-30
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#ifndef ROCK64_ERRNO_H
+#define ROCK64_ERRNO_H
+
+int errno;
+
+#define EINVAL      (-1)    /* Invalid argument */
+#define EBADF       (-2)    /* Invalid file descriptor */
+
+#endif

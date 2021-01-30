8ece2c2957b18e046d8ba7516f621ab538109d2a
Johannes Krottmayer <krjdev@gmail.com>
2021-01-30 10:41:23 +0100

    libc: Add inital internal libc exit function
    NEW FILE(S):
    ./libc/exit.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libc/exit.c
@@ -0,0 +1,22 @@
+/**
+ *
+ * File Name: libc/exit.c
+ * Title    : C Library - Internal
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-30
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+void libc_exit(void)
+{
+    
+}

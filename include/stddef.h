89278006605612c8aa23954529f4ec32d8da6f3f
Johannes Krottmayer <krjdev@gmail.com>
2021-01-30 06:34:17 +0100

    include/stddef.h: Add ssize_t typedef
    Changed file(s):
    ./include/stddef.h
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

--- include/stddef.h
+++ include/stddef.h
@@ -5,7 +5,7 @@
  * Project  : PINE64 ROCK64 Bare-Metal
  * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2021-01-29
- * Modified : 
+ * Modified : 2021-01-30
  * Revised  : 
  * Version  : 0.1.0.0
  * License  : ISC (see LICENSE.txt)
@@ -22,5 +22,6 @@
 #include <stdint.h>
 
 typedef uint64_t size_t;
+typedef int64_t ssize_t;
 
 #endif

fa139c244c505e1d6a278656933c9d591364099b
Johannes Krottmayer <krjdev@gmail.com>
2021-01-30 07:05:57 +0100

    include/unistd.h: Add standard FILENO's; change write() types; add close(), read() and fsync()
    Changed file(s):
    ./include/unistd.h
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

--- include/unistd.h
+++ include/unistd.h
@@ -5,7 +5,7 @@
  * Project  : PINE64 ROCK64 Bare-Metal
  * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2021-01-29
- * Modified : 
+ * Modified : 2021-01-30
  * Revised  : 
  * Version  : 0.1.0.0
  * License  : ISC (see LICENSE.txt)
@@ -22,6 +22,13 @@
 #include <stdint.h>
 #include <stddef.h>
 
-extern int write(int fd, const uint8_t *buf, size_t len);
+#define STDIN_FILENO    0       /* Standard input */
+#define STDOUT_FILENO   1       /* Standard output */
+#define STDERR_FILENO   2       /* Standard error output */
+
+extern ssize_t write(int fd, const void *buf, size_t len);
+extern ssize_t read(int fd, void *buf, size_t len);
+extern int close(int fd);
+extern int fsync(int fd);
 
 #endif

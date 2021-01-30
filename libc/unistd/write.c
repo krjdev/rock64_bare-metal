df2283b7bb9c6b067047dd7ecd9f8c7da61df6d8
Johannes Krottmayer <krjdev@gmail.com>
2021-01-30 10:56:59 +0100

    libc/unistd: Update write function
    CHANGED FILE(S):
    ./libc/unistd/write.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

--- libc/unistd/write.c
+++ libc/unistd/write.c
@@ -5,7 +5,7 @@
  * Project  : PINE64 ROCK64 Bare-Metal
  * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2021-01-26
- * Modified : 
+ * Modified : 2021-01-30
  * Revised  : 
  * Version  : 0.1.0.0
  * License  : ISC (see LICENSE.txt)
@@ -17,8 +17,12 @@
  */
 
 #include <syscall.h>
+#include <unistd.h>
 
-int write(int fd, const uint8_t *buf, size_t len)
+ssize_t write(int fd, const void *buf, size_t len)
 {
-    return sys_write(fd, buf, len);
+    if (fd == STDIN_FILENO)
+        return -1;
+    
+    return SYS_file_write(fd, buf, len);
 }

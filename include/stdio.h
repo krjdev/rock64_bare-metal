e049b7e260e06a6deff67a414a1ebca68e6da4c1
Johannes Krottmayer <krjdev@gmail.com>
2021-01-30 10:46:04 +0100

    include/stdio.h: Add putchar function; add FILE typedef; add Standard filestreams
    CHANGED FILE(S):
    ./include/stdio.h
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

--- include/stdio.h
+++ include/stdio.h
@@ -5,7 +5,7 @@
  * Project  : PINE64 ROCK64 Bare-Metal
  * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2021-01-29
- * Modified : 
+ * Modified : 2021-01-30
  * Revised  : 
  * Version  : 0.1.0.0
  * License  : ISC (see LICENSE.txt)
@@ -19,6 +19,20 @@
 #ifndef ROCK64_STDIO_H
 #define ROCK64_STDIO_H
 
+struct _IO_FILE;
+
+extern struct _IO_FILE *stdin;
+extern struct _IO_FILE *stdout;
+extern struct _IO_FILE *stderr;
+
+typedef struct _IO_FILE FILE;
+
+#ifndef EOF
+#define EOF    (-1)
+#endif
+
 extern int puts(const char *s);
+extern int putc(int c, FILE *stream);
+extern int putchar(int c);
 
 #endif

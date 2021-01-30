e5fc2eb34c9ad398629bbeaa3841cac2a383962f
Johannes Krottmayer <krjdev@gmail.com>
2021-01-30 21:31:17 +0100

    libc/string: Add intial strerror function
    NEW FILE(S):
    ./libc/string/strerror.c
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

---
+++ libc/string/strerror.c
@@ -0,0 +1,33 @@
+/**
+ *
+ * File Name: libc/string/strerror.c
+ * Title    : C Library
+ * Project  : PINE64 ROCK64 Bare-Metal
+ * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
+ * Created  : 2021-01-30
+ * Modified : 
+ * Revised  : 
+ * Version  : 0.1.0.0
+ * License  : ISC (see LICENSE.txt)
+ *
+ * NOTE: This code is currently below version 1.0, and therefore is considered
+ * to be lacking in some functionality or documentation, or may not be fully
+ * tested. Nonetheless, you can expect most functions to work.
+ *
+ */
+
+#include <errno.h>
+
+char *strerror(int errnum)
+{
+    switch (errnum) {
+    case EINVAL:
+        return "Invalid argument";
+    case EBADF:
+        return "Invalid file descriptor";
+    default:
+        errno = EINVAL;
+    }
+    
+    return "Unknown error";
+}

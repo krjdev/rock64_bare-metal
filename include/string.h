a3de2c351ec01350523e95a57ded79ceba4a2da3
Johannes Krottmayer <krjdev@gmail.com>
2021-01-31 06:26:36 +0100

    include/string.h: Add strerror function
    CHANGED FILE(S):
    ./include/string.h
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

--- include/string.h
+++ include/string.h
@@ -5,7 +5,7 @@
  * Project  : PINE64 ROCK64 Bare-Metal
  * Author   : Copyright (C) 2021 Johannes Krottmayer <krjdev@gmail.com>
  * Created  : 2021-01-29
- * Modified : 
+ * Modified : 2021-01-30
  * Revised  : 
  * Version  : 0.1.0.0
  * License  : ISC (see LICENSE.txt)
@@ -19,15 +19,29 @@
 #ifndef ROCK64_STRING_H
 #define ROCK64_STRING_H
 
+/* Compare memory */
 extern int memcmp(const void *m1, const void *m2, int len);
+/* Copy memory */
 extern void *memcpy(void *dst, const void *src, int len);
+/* Set memory */
 extern void *memset(void *buf, const int val, int len);
+
+/* Concatenate strings */
 extern char *strcat(char *dst, const char *src);
+/* Compare strings */
 extern int strcmp(const char *s1, const char *s2);
+/* Copy string */
 extern char *strcpy(char *dst, const char *src);
+/* String length */
 extern int strlen(const char *s);
+/* Concatenate strings */
 extern char *strncat(char *dst, const char *src, int num);
+/* Compare strings */
 extern int strncmp(const char *s1, const char *s2, int num);
+/* Copy string */
 extern char *strncpy(char *dst, const char *src, int num);
+
+/* Return error description */
+extern char *strerror(int errnum);
 
 #endif

2de37ae8c66f44e3569336c4c33000af6327d545
Johannes Krottmayer <krjdev@gmail.com>
2021-01-31 06:56:26 +0100

    libc/string/files.mk: Update local Makefile
    CHANGED FILE(S):
    ./libc/string/files.mk
    
    Signed-off-by: Johannes Krottmayer <krjdev@gmail.com>

--- libc/string/files.mk
+++ libc/string/files.mk
@@ -10,6 +10,7 @@ CSRC_USER	+= libc/string/memset.c
 CSRC_USER	+= libc/string/strcat.c
 CSRC_USER	+= libc/string/strcmp.c
 CSRC_USER	+= libc/string/strcpy.c
+CSRC_USER	+= libc/string/strerror.c
 CSRC_USER	+= libc/string/strlen.c
 CSRC_USER	+= libc/string/strncat.c
 CSRC_USER	+= libc/string/strncmp.c
